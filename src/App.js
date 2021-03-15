9408770b1b38ef0eb5cac1dacd59750beccf59bd
Saquib Khan <ksaquib54@gmail.com>
2021-03-15 18:53:36 +0530

    added unsplash api as well as search feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
+
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  // const getPhotos = useCallback(() => {
+  //   let apiUrl = `https://api.unsplash.com/photos/?`;
+  //   if (query)
+  //     apiUrl = `https://api.unsplash.com/search/photos/?query=${query}`;
+  //   apiUrl += `&page=${page}`;
+  //   apiUrl += `&client_id=${accessKey}`;
+  //   fetch(apiUrl)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const imagesFromApi = data.results ?? data;
+  //       if (page == 1) setImages(imagesFromApi);
+  //       setImages((images) => [...images, ...imagesFromApi]);
+  //     });
+  // }, [page, query]);
+
+  useEffect(() => {
+    getPhotos();
+    // eslint-disable-next-line
+  }, [page]);
+
+  function getPhotos() {
+    let apiUrl = `https://api.unsplash.com/photos/?`;
+    if (query)
+      apiUrl = `https://api.unsplash.com/search/photos/?query=${query}`;
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const imagesFromApi = data.results ?? data;
+        if (page == 1) setImages(imagesFromApi);
+        setImages((images) => [...images, ...imagesFromApi]);
+      });
+  }
+
+  function searchPhotos(e) {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
+  }
+
+  // return an error if there is no access key
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/developers" className="error">
+        Required: Get Your Unsplash API Key First
+      </a>
+    );
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
       </form>
-
-      <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
-          <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length} //This is important field to render the next data
+        next={() => {
+          setPage((page) => page + 1);
+        }}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
